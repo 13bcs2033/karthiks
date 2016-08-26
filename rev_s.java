50cd8234d35829e5124b53f434976801e43e27ae
13bcs2033 <karthiksengodanvijay@gmail.com>
2016-08-26 08:35:00 +0530

    rev_s.java

---
+++ rev_s.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+
+public class rev_s {
+
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		String s1=sc.next();
+		Integer s=Integer.valueOf(s1);
+		
+		System.out.println(s);
+	}
+
+}
